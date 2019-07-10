753febc2154529db3655fe562f82b4b4d3df1998
RichFisher <rich.fishernc@gmail.com>
2019-07-09 21:10:07 -0500

    more scaffolding

---
+++ capstone/ClientApp/src/data/registerData.js
@@ -0,0 +1,49 @@
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import axios from 'axios';
+import apiKeys from './apiKeys';
+
+const DBURL = apiKeys.baseUrl;
+
+axios.interceptors.request.use((request) => {
+  const token = sessionStorage.getItem('token');
+  if (token != null) {
+      request.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return request;
+}, (err) => {
+  return Promise.reject(err);
+});
+
+axios.interceptors.response.use(response => {
+    return response;
+}, errorResponse => {
+   console.error("Blew up")
+});
+
+const createUser = (email, password) => new Promise((resolve, reject) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      resolve();
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+const createUserInDB = (FirstName, LastName, UserName, Uid) => {
+  const newUser = {
+    FirstName,
+    LastName,
+    UserName,
+    Uid
+  }
+  axios.post(`${DBURL}/user`, newUser);
+}
+
+export default {
+  createUser,
+  createUserInDB
+};
